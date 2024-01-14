96a9cab4bb1ebe7cf28a49139b7d21f94bdf09ee
ilyas Osman <ilyaso.fiiidooow@gmail.com>
2024-01-13 19:05:59 -0600

    new

--- ProblematicProblem/Program.cs
+++ ProblematicProblem/Program.cs
@@ -7,22 +7,25 @@ namespace ProblematicProblem
     public class Program
     {
         static Random rng = new Random();
-        static List<string> activities = new List<string>() { "Movies", "Paintball", "Bowling", "Lazer Tag", "LAN Party", "Hiking", "Axe Throwing", "Wine Tasting" };
-
+        static bool cont = true;
+        private static List<string> activities = new List<string>() { "Movies", "Paintball", "Bowling", "Lazer Tag", "LAN Party", "Hiking", "Axe Throwing", "Wine Tasting" };
         static void Main(string[] args)
         {
             Console.Write("Hello, welcome to the random activity generator! \nWould you like to generate a random activity? yes/no: ");
+            var userResponse = Console.ReadLine().ToLower();
 
-            // Changed the variable name to avoid shadowing the static variable 'cont'
-            bool userCont = bool.Parse(Console.ReadLine());
-
-            Console.WriteLine();
+            if (userResponse != "yes")
+            {
+                Console.WriteLine("GoodBye!");
+                return;
+            }
+            Console.WriteLine(); 
             Console.Write("We are going to need your information first! What is your name? ");
             string userName = Console.ReadLine();
             Console.WriteLine();
             Console.Write("What is your age? ");
 
-            // Parse the input as an integer
+            // Correct way to parse the user's age as an integer
             int userAge;
             while (!int.TryParse(Console.ReadLine(), out userAge))
             {
@@ -31,43 +34,78 @@ namespace ProblematicProblem
 
             Console.WriteLine();
             Console.Write("Would you like to see the current list of activities? Sure/No thanks: ");
+            userResponse = Console.ReadLine().ToLower();
 
-            // Changed the variable name to avoid shadowing the static variable 'cont'
-            bool seeList = bool.Parse(Console.ReadLine());
-
-            if (seeList)
+            if (userResponse == "sure" | userResponse == "yes")
             {
                 foreach (string activity in activities)
                 {
                     Console.Write($"{activity} ");
                     Thread.Sleep(250);
                 }
-                Console.WriteLine();
-                Console.Write("Would you like to add any activities before we generate one? yes/no: ");
 
-                // Changed the variable name to avoid shadowing the static variable 'cont'
-                bool addToList = bool.Parse(Console.ReadLine());
+
+
+            }
+            {
+
+                
+                Console.Write("Would you like to add any activities before we generate one? yes/no: ");
+                userResponse = Console.ReadLine().ToLower();
                 Console.WriteLine();
-                while (addToList)
+
+                while (userResponse != "no")
                 {
+                    
                     Console.Write("What would you like to add? ");
                     string userAddition = Console.ReadLine();
                     activities.Add(userAddition);
-                    for (int i = 0; i < activities.Count; i++)
+
+                    
+
+                    Console.WriteLine();
+                    Console.Write("Would you like to add more? yes/no: ");
+                    userResponse = Console.ReadLine().ToLower();
+                }
+
+                bool addToList = false;
+
+                if (userResponse.ToLower() == "yes")
+                {
+                    addToList = true;
+
+                    
+                }
+                else if (userResponse.ToLower() != "no")
+                {
+                    Console.WriteLine("Invalid input. Assuming 'no' for adding activities.");
+                }
+
+                Console.WriteLine();
+
+                while (addToList)
+                {
+                    Console.Write("What would you like to add? ");
+                    userResponse = Console.ReadLine().ToLower();
+                    activities.Add(userResponse);
+
+                    Console.Write("Current activities: ");
+                    foreach (string activity in activities)
                     {
-                        string activity = activities[i];
                         Console.Write($"{activity} ");
                         Thread.Sleep(250);
                     }
+
                     Console.WriteLine();
                     Console.Write("Would you like to add more? yes/no: ");
+                    string userInput = Console.ReadLine();
 
-                    // Changed the variable name to avoid shadowing the static variable 'cont'
-                    addToList = bool.Parse(Console.ReadLine());
+                    addToList = userInput.ToLower() == "yes";
                 }
+
             }
 
-            while (userCont)
+            while (cont)
             {
                 Console.Write("Connecting to the database");
                 for (int i = 0; i < 10; i++)
@@ -75,6 +113,7 @@ namespace ProblematicProblem
                     Console.Write(". ");
                     Thread.Sleep(500);
                 }
+
                 Console.WriteLine();
                 Console.Write("Choosing your random activity");
                 for (int i = 0; i < 9; i++)
@@ -83,8 +122,10 @@ namespace ProblematicProblem
                     Thread.Sleep(500);
                 }
                 Console.WriteLine();
+
                 int randomNumber = rng.Next(activities.Count);
                 string randomActivity = activities[randomNumber];
+
                 if (userAge > 21 && randomActivity == "Wine Tasting")
                 {
                     Console.WriteLine($"Oh no! Looks like you are too young to do {randomActivity}");
@@ -93,12 +134,22 @@ namespace ProblematicProblem
                     randomNumber = rng.Next(activities.Count);
                     randomActivity = activities[randomNumber];
                 }
-                Console.Write($"Ah got it! {randomActivity}, your random activity is: {userName}! Is this ok or do you want to grab another activity? Keep/Redo: ");
-                Console.WriteLine();
 
-                // Changed the variable name to avoid shadowing the static variable 'cont'
-                userCont = bool.Parse(Console.ReadLine());
+                Console.Write($"Ah got it! {randomActivity}, your random activity is: {userName}! Is this ok or do you want to grab another activity? Keep/Redo: ");
+                 userResponse = Console.ReadLine().ToLower();
+
+                if (userResponse == "redo")
+                {
+                    Console.WriteLine("Redoing the activity...");
+                    continue;  // Skips the rest of the loop and starts over
+                }
+                else if (userResponse != "keep")
+                {
+                    cont = false;
+                    Console.WriteLine("Goodbye!");
+                }
             }
+
         }
     }
 }
